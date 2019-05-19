1abd1dc75b5a333de550e78cb3cb0515b27cf319
Katerina <dubovskayakatya56@gmail.com>
2019-05-19 14:54:26 +0300

    Created project

---
+++ src/NewsList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import News from './News.js'
+
+function NewsList({newsList}){
+    const list = newsList.map(news => <News data = {news}/>)
+    return(
+        list
+    )
+}
+
+export default NewsList 
---
+++ src/SourceList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Source from './Source.js';
+
+function SourceList({sourceList}){
+    const list = sourceList.map(source => <Source source = {source}/>)
+    return(
+        list
+    )
+}
+
+export default SourceList 
